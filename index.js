cef4a3a30168c3d4d0a601f9e23e0ddb9b838087
drsalnikov <drsalnikovev@gmail.com>
2017-07-20 14:48:01 +0300

    done

--- index.js
+++ index.js
@@ -1,104 +1,32 @@
-'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const api = require('./api');
+const rpc = require('./rpc');
 
-const http = require('http');
-const queryString = require('querystring');
+let data = require('./users');
+let app = express();
 
-const port = 80;
-const httpServer = http.createServer();
+app.use(bodyParser.json());
 
-
-// 1. Get data from user
-// 2. Request to netology.tomilomark.ru API like client
-// 3. Return json data by http.createServer localhost
-// data ex.{"firstName": "Mark",   "lastName": "Tomilo",   "secretKey": "w45wdgwe5r235rqwefdsg" }
-
-/*
- *
- */
-const serverHandler = (req, res) => {
-  let data = '';
-  let user = {};
-
-  req.on('data', chunk => {
-    data += chunk;
-  });
-
-  req.on('end', () => {
-    let parseData = parse(data, req.headers['content-type']);
-    user.firstName = parseData.firstName || 'mark';
-    user.lastName = parseData.lastName || 'tomilo';
-    let postData = JSON.stringify(user);
-    clientResponse(postData, user)
-      .then(
-        response => {
-          res.writeHead(200, 'OK', {'Content-Type': 'application/json'});
-          res.write(JSON.stringify(response));
-          res.end();
-        }
-      )
-      .catch(
-        err => {
-          console.dir(err);
-        }
-      );
-  });
-};
-
-/*
- *
- */
-const clientResponse = (postData, user) => {
-  return new Promise(function(resolve, reject) {
-    let options = {
-      hostname: 'netology.tomilomark.ru',
-      port: 80,
-      path: '/api/v1/hash',
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Firstname': user.firstName
-      }
-    };
-    let request = http.request(options, (response) => {
-      let data = '';
-      response.on('data', (chunk) => {
-        data += chunk;
-      });
-      response.on('end', () => {
-        user.secretKey = JSON.parse(data).hash;
-        resolve(user);
-      });
-      response.on('error', (e) => {
-        reject(`problem with request: ${e.message}`);
-      });
-    });
-    request.write(postData);
-    request.end();
-  });
-};
-
-/*
- *
- */
-const parse = (data, contentType) => {
-  let result;
-  switch (contentType) {
-    case 'application/json':
-      result = JSON.parse(data);
-      break;
-    case 'application/x-www-form-urlencoded':
-      result = queryString.parse(data);
-      break;
-  }
-  return result;
-};
-
-//------------------
-
-httpServer.on('error', err => console.error(err));
-httpServer.on('request', serverHandler);
-httpServer.on('listening', () => {
-  console.log('Start HTTP on port %d', port);
+app.use((req, res, next) => {
+  req.users = data;
+  next();
 });
 
-httpServer.listen(port);
+app.use('/api', api);
+app.use('/rpc', rpc);
+
+app.all('*', (req, res) => {
+  res.send(`
+  1. /api/users
+  2. /rpc`);
+});
+
+app.use(function(err, req, res, next) {
+  //console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(3000, () =>{
+  console.log('Server start');
+});
