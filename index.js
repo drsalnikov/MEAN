597a6190e0cc2c6272a24fa2c7ccaed55aa1f49b
drsalnikov <drsalnikovev@gmail.com>
2017-07-20 14:49:08 +0300

    done

--- index.js
+++ index.js
@@ -1,29 +1,57 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const api = require('./api');
-const rpc = require('./rpc');
 
-let data = require('./users');
 let app = express();
 
 app.use(bodyParser.json());
 
+// extra
 app.use((req, res, next) => {
-  req.users = data;
-  next();
+  // header = key
+  if(req.get('header') === 'key'){
+    next();
+  } else {
+    res.status(401).end();
+  }
 });
 
-app.use('/api', api);
-app.use('/rpc', rpc);
+// 1
+app.get("/", (req, res) => {
+  res.status(200).send(`Hello, Express.js`);
+});
+
+// 2
+app.get("/hello", (req, res) => {
+  res.status(200).send(`Hello stranger!`);
+});
+
+// 3
+app.get("/hello/:name*", (req, res) => {
+  let name = req.params.name;
+  if (name) {
+    res.status(200).send(`Hello, ${name}!`);
+  }
+});
+
+// 4
+app.all("/sub/*", (req, res) => {
+  res.status(200).send(`You requested URI: ${req.protocol}://${req.get('host')}${req.originalUrl}`);
+});
+
+// 5
+app.post("/post", (req, res) => {
+  if (JSON.stringify(req.body) !== '{}') {
+    res.status(200).json(req.body);
+  } else {
+   res.status(404).send('NOT FOUND'); 
+  }
+});
 
 app.all('*', (req, res) => {
-  res.send(`
-  1. /api/users
-  2. /rpc`);
+  res.send(`Wrong request!`);
 });
 
 app.use(function(err, req, res, next) {
-  //console.error(err.stack);
   res.status(500).send('Something broke!');
 });
 
