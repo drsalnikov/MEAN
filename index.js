be3aaf94b8ea79808c4f335fcf2563f042eabc6f
drsalnikov <drsalnikovev@gmail.com>
2017-07-20 14:46:17 +0300

    done

--- index.js
+++ index.js
@@ -1,143 +1,104 @@
 'use strict';
 
-//----------------------------------------------------
+const http = require('http');
+const queryString = require('querystring');
 
-const crypto = require('crypto');
-const hash = crypto.createHash('md5');
-const fs = require('fs');
+const port = 80;
+const httpServer = http.createServer();
 
-const Readable = require('stream').Readable;
-const Writable = require('stream').Writable;
-const Transform = require('stream').Transform;
 
-let iFilename = 'input.txt';
-let oFilename = 'output.txt';
-let textToCrypt = '123';
+// 1. Get data from user
+// 2. Request to netology.tomilomark.ru API like client
+// 3. Return json data by http.createServer localhost
+// data ex.{"firstName": "Mark",   "lastName": "Tomilo",   "secretKey": "w45wdgwe5r235rqwefdsg" }
 
-const input = fs.createReadStream(iFilename, {
-  flags: 'a+',
-  encoding: 'utf8',
-  fd: null,
-  mode: 0o666,
-  autoClose: true
+/*
+ *
+ */
+const serverHandler = (req, res) => {
+  let data = '';
+  let user = {};
+
+  req.on('data', chunk => {
+    data += chunk;
+  });
+
+  req.on('end', () => {
+    let parseData = parse(data, req.headers['content-type']);
+    user.firstName = parseData.firstName || 'mark';
+    user.lastName = parseData.lastName || 'tomilo';
+    let postData = JSON.stringify(user);
+    clientResponse(postData, user)
+      .then(
+        response => {
+          res.writeHead(200, 'OK', {'Content-Type': 'application/json'});
+          res.write(JSON.stringify(response));
+          res.end();
+        }
+      )
+      .catch(
+        err => {
+          console.dir(err);
+        }
+      );
+  });
+};
+
+/*
+ *
+ */
+const clientResponse = (postData, user) => {
+  return new Promise(function(resolve, reject) {
+    let options = {
+      hostname: 'netology.tomilomark.ru',
+      port: 80,
+      path: '/api/v1/hash',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Firstname': user.firstName
+      }
+    };
+    let request = http.request(options, (response) => {
+      let data = '';
+      response.on('data', (chunk) => {
+        data += chunk;
+      });
+      response.on('end', () => {
+        user.secretKey = JSON.parse(data).hash;
+        resolve(user);
+      });
+      response.on('error', (e) => {
+        reject(`problem with request: ${e.message}`);
+      });
+    });
+    request.write(postData);
+    request.end();
+  });
+};
+
+/*
+ *
+ */
+const parse = (data, contentType) => {
+  let result;
+  switch (contentType) {
+    case 'application/json':
+      result = JSON.parse(data);
+      break;
+    case 'application/x-www-form-urlencoded':
+      result = queryString.parse(data);
+      break;
+  }
+  return result;
+};
+
+//------------------
+
+httpServer.on('error', err => console.error(err));
+httpServer.on('request', serverHandler);
+httpServer.on('listening', () => {
+  console.log('Start HTTP on port %d', port);
 });
-const output = fs.createWriteStream(oFilename, {
-  flags: 'w',
-  defaultEncoding: 'utf8',
-  fd: null,
-  mode: 0o666,
-  autoClose: true
-});
 
-
-
-
-class MyTransform extends Transform {
-  constructor(options) {
-    super(options);
-  }
-
-  _transform(chunk, encoding, callback) {
-    this.push(hash.update(chunk).digest('hex'));
-    callback();
-  }
-}
-
-// prepare
-fs.writeFileSync(iFilename, textToCrypt);
-
-
-
-// 1
-//input.pipe(hash).pipe(output);
-//hash.pipe(process.stdout);
-
-// 2
-let mt = new MyTransform();
-//input.pipe(mt).pipe(output);
-//mt.pipe(process.stdout);
-
-
-// appendix -----------------------------------------
-
-class ApdxReadable extends Readable {
-  constructor(options = {}) {
-    options.objectMode = true;
-    super(options);
-  }
-
-  _read(size) {
-    setTimeout( () => {
-      let data = Math.floor(Math.random() * 10);
-      this.push(data);
-    }, 100);
-  }
-}
-
-class ApdxWritable extends Writable {
-  constructor(options = {}) {
-    options.objectMode = true;
-    super(options);
-  }
-
-  _write(chunk, encoding, done) {
-    console.log(chunk);
-    done();
-  }
-}
-
-class ApdxTransform extends Transform {
-  constructor(options ={}) {
-    options.objectMode = true;
-    super(options);
-  }
-
-  _transform(chunk, encoding, callback) {
-    switch(chunk) {
-      case 0 :
-        chunk = 'zero';
-        break;
-      case 1 :
-        chunk = 'ichi';
-        break;
-      case 2 :
-        chunk = 'ni';
-        break;
-      case 3 :
-        chunk = 'san';
-        break;
-      case 4 :
-        chunk = 'shi';
-        break;
-      case 5 :
-        chunk = 'go';
-        break;
-      case 6 :
-        chunk = 'rok';
-        break;
-      case 7 :
-        chunk = 'nana';
-        break;
-      case 8 :
-        chunk = 'achi';
-        break;
-      case 9 :
-        chunk = 'kyuu';
-        break;
-      case 10 :
-        chunk = 'juu';
-        break;
-    }
-    this.push(chunk);
-    setTimeout(() => {
-      callback();
-      }, 1000);
-  }
-}
-
-
-let ar = new ApdxReadable();
-let aw = new ApdxWritable();
-let at = new ApdxTransform();
-
-ar.pipe(at).pipe(aw);
+httpServer.listen(port);
